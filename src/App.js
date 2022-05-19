097e5a757709dac5db0bd1a71692d06d7b21aef8
Moussa Ibrahim <moussaebrahem32@gmail.com>
2022-05-19 20:17:08 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <main className="App">
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
