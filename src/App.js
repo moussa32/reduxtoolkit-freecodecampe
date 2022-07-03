c38ccfe0354c84aba8fc78c901e6f9b09dc8c817
mozes32 <moussaebrahem32@gmail.com>
2022-07-03 20:39:40 +0200

    feat: added async store

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { calculateTotals } from "./features/cart/cartSlice";
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
 import Modal from "./components/Modal";
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 import "./styles.css";
 
 export default function App() {
-  const { cartItems } = useSelector((store) => store.cart);
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
   const { isOpen } = useSelector((store) => store.modal);
   const dispatch = useDispatch();
 
@@ -15,8 +15,17 @@ export default function App() {
     dispatch(calculateTotals());
   }, [cartItems]);
 
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
   return (
     <main className="App">
+      {isLoading && (
+        <div className="loading">
+          <h1>Loadding....</h1>
+        </div>
+      )}
       {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
